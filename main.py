5bb708746b2d7345a440241a5d1d1a0e43aec6b7
matvik777 <2805matvik4@gmail.com>
2025-01-26 00:55:27 +0300

    B: добавлен circleshape.py

--- main.py
+++ main.py
@@ -3,26 +3,29 @@
 # throughout this file
 import pygame
 from constants import *
+
 def main():
     pygame.init()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  # Создаем окно
     pygame.display.set_caption("Asteroids Game")  # Устанавливаем заголовок окна
+    clock = pygame.time.Clock()  # Создаем объект Clock
+    dt = 0  # Переменная для хранения delta time
     while True:
-    	for event in pygame.event.get():
-    		if event.type == pygame.QUIT:
-        		return
-    
-    screen.fill(black)
-    pygame.display.flip()
-   
-   
-   
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill((0,0,0))  # Теперь внутри цикла
+        pygame.display.flip()  # Теперь внутри цикла
+        dt = clock.tick(60)   # Переводим миллисекунды в секунды
+ 
     print('''
-	
- 	Starting asteroids!
-	Screen width: 1280
-	Screen height: 720	
-	''')
+    
+    Starting asteroids!
+    Screen width: 1280
+    Screen height: 720    
+    ''')
 
 if __name__ == "__main__":
     main()
