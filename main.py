b1cf7da9338dd2184dfd6667769f1c9d2519210a
matvik777 <2805matvik4@gmail.com>
2025-01-27 01:00:09 +0300

    C: Закончил разработку игрока

--- main.py
+++ main.py
@@ -1,31 +1,39 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
+from player import Player
+
 
 def main():
     pygame.init()
-
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  # Создаем окно
-    pygame.display.set_caption("Asteroids Game")  # Устанавливаем заголовок окна
-    clock = pygame.time.Clock()  # Создаем объект Clock
-    dt = 0  # Переменная для хранения delta time
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    
+    dt = 0
+    
+    
+    updatables = pygame.sprite.Group()
+    drawbles = pygame.sprite.Group()
+    
+    Player.containers = (updatables, drawbles)
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill((0,0,0))  # Теперь внутри цикла
-        pygame.display.flip()  # Теперь внутри цикла
-        dt = clock.tick(60)   # Переводим миллисекунды в секунды
- 
-    print('''
-    
-    Starting asteroids!
-    Screen width: 1280
-    Screen height: 720    
-    ''')
+        updatables.update(dt)
+        
+        screen.fill("black")
+        for sprite  in drawbles:
+            sprite.draw(screen)
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
